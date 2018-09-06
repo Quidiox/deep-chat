25289f1b0e9ed3035ad9aa3cb6a0cd0140410419
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-09-06 16:13:54 +0300

    socket.io add and connection to server ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client'
 
 class App extends Component {
   render() {
-    return (
-      <div>Hello World!</div>
-    )
+    const socket = io.connect('http://localhost:3005')
+    console.log(socket)
+    return <div>Hello World!</div>
   }
 }
 
