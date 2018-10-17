8dd4d1cdeae289ee9c9912009050affb55c23796
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-10-17 14:57:23 +0300

    project structure refactoring, new packages installed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react'
-import io from 'socket.io-client'
+import Login from './auth/Login'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.socket = io('http://localhost:3005', { path: '/server' })
-  }
   render() {
-    this.socket.emit('message', 'hello world!!!111!!')
-    this.socket.on('hi', msg => console.log(msg))
-    return <div>Hello World!</div>
+    return <Login />
   }
 }
 
---
+++ src/pages/socket.io/Chat.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import io from 'socket.io-client'
+
+class Chat extends Component {
+  constructor(props) {
+    super(props)
+    this.socket = io('http://localhost:3005', { path: '/server' })
+  }
+  render() {
+    this.socket.on('connect', () => {
+      this.socket.emit('message', 'hello world!!!111!!')
+      this.socket.on('hi', msg => console.log(msg))
+    })
+    return <div>Hello World!</div>
+  }
+}
+
+export default Chat
