bffc8f039f7598d02e42e57d2edc7a654a9ab9e2
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-11-13 16:25:13 +0200

    styled-components/macro import fix and min-height of dropdown 20px

--- src/components/blocks/StyledHeader/DropDownMenu.js
+++ src/components/blocks/StyledHeader/DropDownMenu.js
@@ -5,7 +5,7 @@ const DropDownMenu = styled(UL)`
   z-index: 1000;
   width: ${props => props.width || '100px'};
   height: ${props => props.height || 'auto'};
-  min-height: 50px;
+  min-height: 20px;
   border: 1px solid black;
   right: ${props => (props.right ? '0' : '')};
   margin: 5px;
--- src/components/blocks/StyledHeader/DropDownMenuItem.js
+++ src/components/blocks/StyledHeader/DropDownMenuItem.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 import LI from '../../elements/LI'
 
 const DropDownMenuItem = styled(LI)``
