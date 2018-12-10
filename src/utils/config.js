fd87e244b4496df9d8299b1cb1966d28cf6791e8
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-12-10 18:44:16 +0200

    remove utils and change to csp to allow connection to heroku

--- src/utils/config.js
+++
@@ -1,9 +0,0 @@
-let API_URL = ''
-if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV, process.env.REACT_APP_HEROKU_API)
-  API_URL = process.env.REACT_APP_HEROKU_API
-} else {
-  console.log(process.env.NODE_ENV, process.env.REACT_APP_HEROKU_API)
-  API_URL = process.env.REACT_APP_LOCAL_API
-}
-export default API_URL
