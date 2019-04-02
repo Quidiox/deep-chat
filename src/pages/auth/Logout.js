58943ba545cc08f829595a5ca761459a95283c12
Quidiox <33523865+Quidiox@users.noreply.github.com>
2019-04-02 10:50:33 +0300

    more testing related libraries, redirect after logout

--- src/pages/auth/Logout.js
+++ src/pages/auth/Logout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import { requestLogoutUser } from '../../reducers/userReducer'
 
 const Logout = ({ requestLogoutUser }) => {
   requestLogoutUser()
-  return <></>
+  return <Redirect to="/" />
 }
 
 const mapDispatchToProps = { requestLogoutUser }
